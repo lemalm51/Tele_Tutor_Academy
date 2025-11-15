3209cb51beeea9918d27895bc3fdf59ed207ed01
Your Name <your_email@example.com>
2025-11-15 13:41:39 +0300

    connected successfully

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ import connectDB from './configs/mongodb.js';
 import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
-// import connectCloudinay from './configs/cloudinary.js';
-// import courseRouter from './routes/courseRoute.js';
-// import userRouter from './routes/userRoutes.js';
+import connectCloudinay from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 // initialize express 
 const app = express();
@@ -17,7 +17,7 @@ const app = express();
 
 await connectDB();
 
-// await connectCloudinay();
+await connectCloudinay();
 
 
 // middleware
@@ -32,9 +32,9 @@ app.get('/', (req,res)=>{res.send("STEMA API is working fine!")})
 
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter);
-// app.use('/api/course', express.json(), courseRouter);
-// app.use('/api/user', express.json(), userRouter);
-// app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 
 
