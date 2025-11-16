1485ceae3d733d7136821c9b88a21e453cb2412a
Your Name <your_email@example.com>
2025-11-16 10:40:22 +0300

    trying to reach

--- server/server.js
+++ server/server.js
@@ -2,12 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
-import educatorRouter from './routes/educatorRoutes.js';
-import { clerkMiddleware } from '@clerk/express';
-import connectCloudinay from './configs/cloudinary.js';
-import courseRouter from './routes/courseRoutes.js';
-import userRouter from './routes/userRoutes.js';
+import { clerkWebhooks,  } from './controllers/webhooks.js';
+
 
 // initialize express 
 const app = express();
@@ -17,24 +13,25 @@ const app = express();
 
 await connectDB();
 
-await connectCloudinay();
+// await connectCloudinay();
 
 
 // middleware
 
 app.use(cors());
 
-app.use(clerkMiddleware())
+// app.use(clerkMiddleware())
 
 
 // Routes
-app.get('/', (req,res)=>{res.send(" API is working fine!")})
+app.get('/', (req,res)=>{res.send(" I DO NOT GIVE UP API is working fine!")})
 
 app.post('/clerk', express.json(), clerkWebhooks)
-app.use('/api/educator', express.json(), educatorRouter);
-app.use('/api/course', express.json(), courseRouter);
-app.use('/api/user', express.json(), userRouter);
-app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
+
+// app.use('/api/educator', express.json(), educatorRouter);
+// app.use('/api/course', express.json(), courseRouter);
+// app.use('/api/user', express.json(), userRouter);
+// app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 
 
